7db2bc324cfdc39796d0c7620246515b6dea140e
Victor Borshchov <victor.borshchov@gmail.com>
2019-09-18 00:28:42 +0300

    add task update, delete and get_all endpoints

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -6,17 +6,37 @@ exports.task_create = function(req, res, next) {
     description: req.body.description
   });
 
-  task.save(function(err) {
+  task.save(function(err, task) {
     if (err) {
       return next(err);
     }
-    res.send("Task Created successfully");
+    res.send(task);
   });
 };
 
+exports.task_get_all = function(req, res) {
+    Task.find({}, (err, tasks) => {
+        res.send(tasks);
+    });
+};
+
 exports.task_show = function(req, res, next) {
-  Task.findById(req.params.id, function(err, product) {
+  Task.findById(req.params.id, function(err, task) {
     if (err) return next(err);
-    res.send(product);
+    res.send(task);
+  });
+};
+
+exports.task_update = function(req, res) {
+  Task.findByIdAndUpdate(req.params.id, { $set: req.body }, function( err, task ) {
+    if (err) return next(err);
+    res.send(task);
+  });
+};
+
+exports.task_delete = function(req, res) {
+  Task.findByIdAndRemove(req.params.id, function(err) {
+    if (err) return next(err);
+    res.send("Deleted successfully!");
   });
 };
--- routes/task.route.js
+++ routes/task.route.js
@@ -4,6 +4,9 @@ const router = express.Router();
 // Require the controllers WHICH WE DID NOT CREATE YET!!
 const task_controller = require("../controllers/task.controller");
 
+router.get("/", task_controller.task_get_all);
 router.post("/", task_controller.task_create);
 router.get("/:id", task_controller.task_show);
+router.put("/:id", task_controller.task_update);
+router.delete("/:id", task_controller.task_delete);
 module.exports = router;
