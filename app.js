b9fec9855dc312dd11bd65483d6720fd98890160
Victor Borshchov <victor.borshchov@gmail.com>
2019-09-17 23:55:26 +0300

    use body parser

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const task = require('./routes/task.route'); // Imports routes for the products
+// initialize our express app
+const app = express();
+
 // Set up mongoose connection
 const mongoose = require('mongoose');
 let dev_db_url = "mongodb://victor:victor1db2password@ds341247.mlab.com:41247/axis_tracking";
@@ -7,13 +14,8 @@ mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-const express = require("express");
-const bodyParser = require("body-parser");
-
-const task = require('./routes/task.route'); // Imports routes for the products
-// initialize our express app
-const app = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/tasks", task);
 
 let port = 1234;
